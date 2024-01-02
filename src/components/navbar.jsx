f8b55067b8fc4183f73565439a801bf429d67829
Rohan <walkerrandy930@gmail.com>
2024-01-02 08:20:51 +0545

    first push

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+export const Navbar = () =>{
+    return <header className="w-full">
+        <div className="container mx-auto flex justify-between">
+            <div>Miralou</div>
+            <nav>Links</nav>
+            <nav>Actions</nav>
+        </div>
+    </header>
+}
